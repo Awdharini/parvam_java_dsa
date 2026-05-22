fb965b3a7b2f73043ba9502bb893f4b3b3a81235
Awdharini <190753477+Awdharini@users.noreply.github.com>
2026-05-22 16:53:39 +0530

    Delete src directory

--- src/main/java/com/repository/CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.booking_system.entity.Customer;
-
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
-}
